177de1a2107435211febc08d48adc24d8c552ecc
yozz <yozz@Manava.local>
2015-08-18 21:53:43 -0400

    GitHub Plugin Updater

--- startup-cpt-projects.php
+++ startup-cpt-projects.php
@@ -1,10 +1,34 @@
 <?php
 /*
-Plugin Name: StartUp Projects Custom Post Type
+Plugin Name: StartUp Projects
 Description: Le plugin pour activer le Custom Post Projects
 Author: Yann Caplain
-Version: 1.1.0
+Version: 1.2.0
 */
+
+//GitHub Plugin Updater
+function startup_reloaded_projects_updater() {
+	include_once 'lib/updater.php';
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if ( is_admin() ) {
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'startup-cpt-projects',
+			'api_url' => 'https://api.github.com/repos/yozzi/startup-cpt-projects',
+			'raw_url' => 'https://raw.github.com/yozzi/startup-cpt-projects/master',
+			'github_url' => 'https://github.com/yozzi/startup-cpt-projects',
+			'zip_url' => 'https://github.com/yozzi/startup-cpt-projects/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+		new WP_GitHub_Updater( $config );
+	}
+}
+
+add_action( 'init', 'startup_reloaded_projects_updater' );
 
 //CPT
 function startup_reloaded_projects() {
